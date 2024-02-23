de19620df38ad2dfe7bf0ded56c776a4e3916d6c
unknown <joao.caglianone@gmail.com>
2024-02-22 23:16:25 -0300

    componentizacao da pagina de login e footer

---
+++ src/components/rodape.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Rodape(props) {
+
+     return (
+        <>
+            
+            <footer className="footer-form">
+                <p>&copy; 2024 Barbearia Mix. Todos os direitos reservados.</p>
+                <p>Email: contato@minhaempresa.com</p>
+                
+                <nav>
+                    <a href="/sobre">Sobre Nós</a>
+                    | <a href="/contato">Contato</a>
+                    | <a href="/termos">Termos de Serviço</a>
+                </nav>
+            </footer>
+        </>
+    )
+}
+
+export default Rodape;
