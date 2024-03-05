acbac8480635b9ccaa0b35dcfbb4544bf22a5d65
Bernardo Tomaz <bernardo.tomaz@hotmail.com>
2024-03-05 20:59:47 -0300

    inserção de rotas na redefinicao de senha

---
+++ src/components/redefinicao-senha.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Redefin = () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <div id="titulo">
+        <p className="h1" id="tBarbearia">
+          barbearia
+        </p>
+        <p className="h1" id="tMix">
+          mix
+        </p>
+      </div>
+
+      <div className="login-container" id="login-campos">
+        <figure className="logo"> </figure>
+
+        <form className="redefinicaoSenha">
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            required
+            className="email"
+            id="email"
+          />
+          <div className="btn-forgottEmail">
+            <button type="button" id="forgottEmailC">
+              Cancelar
+            </button>
+            <button type="button" id="forgottEmailCC">
+              Continuar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Redefin;
